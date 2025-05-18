a132589415581f48584a074d1cc29757c687d0f8
KentseMatshira <154395251+KentseMatshira@users.noreply.github.com>
2025-05-18 20:27:35 +0200

    Fixed loading state bug + displayed data on page

--- My App/src/components/FetchApi.jsx
+++ My App/src/components/FetchApi.jsx
@@ -1,38 +1,51 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function FetchAllBlogPosts() {
-  const { error, setError } = useState(null);
-  const { blogPost, setBlogPost } = useState(null);
-  const { isloading, setIsLoading } = useState(false);
+  // State management for Blogs, errors and loading
+  const [error, setError] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async function loadBlogs() {
-      try {
-        setIsLoading(true);
-        // Fetching blog posts dat
-        const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const json = await data.json();
-
-        setBlogPost(json);
+    async function fetchData() {
+        // When fetching data a loading state is enabled
+       // Fetch the blog posts
+       const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      setBlogs(data);
         console.log(data);
       } catch (err) {
         setError(err);
       } finally {
-        setIsLoading(false);
+        setLoading(false);
       }
     }
-    loadBlogs();
+    fetchData();
   }, []);
 
-  if (isloading) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {"Data fetching failed"}</div>;
   }
 
   return (
     <div>
       <h1>Posts</h1>
-      <h2></h2>
-      <h3></h3>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id} style={{ marginBottom: "20px" }}>
+            <div style={{ fontWeight: "bold" }}>
+              {blog.id}. {blog.title}
+            </div>
+            <div>{blog.body}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+
